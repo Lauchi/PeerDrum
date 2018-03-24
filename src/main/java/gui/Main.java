ade9cd0b4b799699a7d3224bfa3f7a716ef2e157
christopher.hansen <christopher.hansen@porschedigitallab.com>
2018-03-24 18:14:19 +0100

    added fxml gui and did some deletions

---
+++ src/main/java/gui/Main.java
@@ -0,0 +1,32 @@
+package gui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Peerdrum.fxml"));
+		Scene scene = new Scene(root);
+		
+		// Title
+		primaryStage.setTitle("Peerdrum");
+		
+		// Szene setzen
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+         
+        // Stage anzeigen
+        primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
