3d90024e401a2a51cfb9fccefe4b870001179758
Elias Aebi <eyelash@users.noreply.github.com>
2018-03-25 04:36:15 +0200

    send MIDI events

--- src/main/java/Domain/MidiSender.java
+++ src/main/java/Domain/MidiSender.java
@@ -1,7 +1,16 @@
 package Domain;
 
+import javax.sound.midi.*;
+
 public class MidiSender {
     public void Send(TimeStep timeStep) {
-        //Send Midi
+        if (timeStep.isSet) {
+            try (Receiver receiver = MidiSystem.getReceiver()) {
+                ShortMessage message = new ShortMessage(ShortMessage.NOTE_ON, 0, 60, 93);
+                receiver.send(message, -1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
