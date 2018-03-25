71c14f14e208aaf72520f569baa04a2e453060e4
christopher.hansen <christopher.hansen@porschedigitallab.com>
2018-03-25 01:41:00 +0100

    ui is now updatable

--- src/main/java/gui/GridController.java
+++ src/main/java/gui/GridController.java
@@ -1,9 +1,16 @@
 package gui;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.GridPane;
 
 
 public class GridController {
+	
+	@FXML
+	private GridPane panelGrid;
 
     /**
      * Initializes the controller class. This method is automatically called
@@ -18,6 +25,13 @@ public class GridController {
 	 */
 	@FXML
 	private void handlePanelClick() {
+		ObservableList<Node> childlist = panelGrid.getChildren();
+		
+		for (Node node : childlist) {
+			ToggleButton button = (ToggleButton) node;
+			System.out.print(button.isSelected() + " ");
+		}
+		
 	    System.out.println("Button was presssed");
 	}
 
