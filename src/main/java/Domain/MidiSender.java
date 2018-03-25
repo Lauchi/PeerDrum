557e15222ad30fd7dd581680a02ce9193cf356bc
Simon Heiss <simonheiss87@gmail.com>
2018-03-25 08:42:18 +0200

    multiple shit working

--- src/main/java/Domain/MidiSender.java
+++ src/main/java/Domain/MidiSender.java
@@ -6,8 +6,8 @@ public class MidiSender {
     public void Send(TimeStep timeStep, int note) {
         if (timeStep.isSet) {
             try (Receiver receiver = MidiSystem.getReceiver()) {
-                ShortMessage message = new ShortMessage(ShortMessage.NOTE_ON, 0, 0, 100);
-                ShortMessage messageOff = new ShortMessage(ShortMessage.NOTE_OFF, 0, 0, 100);
+                ShortMessage message = new ShortMessage(ShortMessage.NOTE_ON, 0, 0 + note, 100);
+                ShortMessage messageOff = new ShortMessage(ShortMessage.NOTE_OFF, 0, 0 + note, 100);
                 receiver.send(message, -1);
                 receiver.send(messageOff, -1);
             } catch (Exception e) {
